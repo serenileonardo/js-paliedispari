5796ae1586614953deabd1a173773d9a15868bbe
serenileonardo <serenileonardo.web@gmail.com>
2025-12-05 15:08:58 +0100

    feat: exercise 1 finished

---
+++ script.js
@@ -0,0 +1,31 @@
+/*
+Ciao ragazzi,
+Esercizio di oggi:
+nome repo: js-paliedispari
+
+Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+
+
+*/
+
+const parolaUtente = prompt("Inserisci una parola: ");
+
+function palindroma(parola) {
+
+    const parolaInvertita = parola.split("").reverse().join("");
+
+    if (parola === parolaInvertita) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+  if (palindroma(parolaUtente)) {
+        console.log("La parola è palindroma!");
+    } else {
+        console.log("La parola NON è palindroma.");
+    }
+
